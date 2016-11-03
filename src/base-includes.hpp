d67b7742b4511f3e32307e766846ae98f12607c3
Philip Woods <elzairthesorcerer@gmail.com>
2016-11-03 14:21:33 -0400

    Made binary go to root of output directory

--- src/base-includes.hpp
+++ src/base-includes.hpp
@@ -5,9 +5,6 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-//#ifdef GLM_HAS_CXX11_STL   // Hack to get GLM's
-//#undef GLM_HAS_CXX11_STL  // hash support working
-//#endif
 #include <glm/gtx/hash.hpp>
 
 #define GLFW_INCLUDE_VULKAN
@@ -22,11 +19,5 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-//#define STB_IMAGE_IMPLEMENTATION
-//#include "3rdparty/stb_image.h"
-
-//#define TINYOBJLOADER_IMPLEMENTATION
-//#include "3rdparty/tiny_obj_loader.h"
 
 #endif
